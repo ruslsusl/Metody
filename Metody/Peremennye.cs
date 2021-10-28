6441b3123caee1e1d6e900bc85fa226cb5c02681
Ruslan Aleyev <aleyev.rus@gmail.com>
2021-10-28 20:55:39 +0300

    Added2Classes

---
+++ Metody/Peremennye.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metody
+{
+
+     class Peremennye
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetFormulaResult(3, 5));
+
+
+                }
+         static double GetFormulaResult(double A, double B)
+        {
+            double C = (5 * A + B * B) / (B - A);
+            return C;
+        }
+        static string ChangeStrings(string a, string b)
+        {
+            string tmp = a;
+            a = b;
+            b = tmp;
+            return $"a = {a}, b = {b}";
+           
+        }
+         static double CalculateDivision(double a, double b)
+        { 
+           double c = a / b;
+            return c;
+        }
+         static double CalculateDivisionRemainder(double a, double b)
+        {
+            double c = a % b;
+            return c;
+        }
+         static double SolveLinearEquation(double A, double B, double C)
+        {
+            double X = (C - B) / A;
+            return X;
+        }
+         static string GetEquationStraight(double x1, double y1, double x2, double y2)
+        {
+
+            double a = (y2 - y1) / (x2 - x1);
+            double b = -((x1 * y2) - (x2 * y1)) / (x2 - x1);
+            return $"y = {a}*x+{b}";
+        }
+
+
+
+
+
+
+
+
+
+       
+        
+
+    }
+}
---
+++ Metody/StructuryVetvleniya.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metody
+{
+    class StructuryVetvleniya
+    {
+        
+        static double CalculateTwoNumbers(double A, double B)
+        {
+            double result = 0;
+            if (A > B)
+            {
+                result = A + B;
+            }
+
+            else if (A < B)
+
+            {
+                result = A - B;
+            }
+
+
+            else
+            {
+                result = A * B;
+            }
+            return result;
+        }
+        static string ChooseQuarter(double X, double Y)
+        {
+            string result = "";
+            if (X > 0 && Y > 0)
+
+            {
+                result = "Первая четверть";
+            }
+
+
+            if (X < 0 && Y > 0)
+
+            { result = "Вторая четверть"; }
+
+            if (X < 0 && Y < 0)
+
+            { result = "Третья четверть"; }
+
+            if (X > 0 && Y < 0)
+
+            {
+                result = "Четвертая четверть";
+            }
+
+
+            else if (X == 0 || Y == 0)
+            {
+                result = "Точка на Оси";
+            }
+            return result;
+        }
+        static string PutNumbersInOrder(double A, double B, double C)
+        {
+            string result = "";
+            if (A > B)
+            {
+
+                if (B > C)
+
+                {
+                    result = $"{A},{B},{C}";
+                }
+
+
+
+                else if (C < A)
+                {
+                    result = $"{A},{C},{B}";
+                }
+                else
+                {
+                    result = $"{C},{A},{B}";
+                }
+            }
+            else
+            {
+                if (A > C)
+                {
+                    result = $"{B},{A},{C}";
+                }
+                else if (B > C)
+                {
+                    result = $"{B},{C},{A}";
+                }
+                else
+                {
+                    result = $"{C},{B},{A}";
+                }
+
+            }
+            return result;
+        }
+        static string SolveKvadratichnoeUravneniye(double A, double B, double C)
+        {
+            string result = "";
+            if (B * B - 4 * A * C > 0)
+            {
+
+                double X1 = ((-B + Math.Sqrt(B * B - 4 * A * C)) / 2 * A);
+                double X2 = ((-B - Math.Sqrt(B * B - 4 * A * C)) / 2 * A);
+
+
+
+                result = $"Ответ: X1 = {X1}, Ответ: X2 =  { X2}";
+
+            }
+            if (B * B - 4 * A * C == 0)
+            {
+                double X = -B / 2 * A;
+                result = $"Ответ: X ={X}";
+
+            }
+            else if (B * B - 4 * A * C < 0)
+            {
+
+                result = "Решения нет";
+            }
+            return result;
+        }
+        static public string WriteNumberInWords(int a)
+        {
+            int q = a / 10;
+            int w = a % 10;
+            string s = "";
+
+
+
+            if (q == 1)
+            {
+                switch (a)
+                {
+                    case 10:
+
+                        {
+                            s = "Десять";
+
+
+
+
+
+
+
+
+
+                        }
+                        break;
+
+
+                    case 11:
+                        {
+
+                            s = "Одиннадцать";
+                        }
+                        break;
+
+
+                    case 12:
+                        {
+                            s = "Двенадцать";
+                        }
+                        break;
+
+                    case 13:
+                        {
+                            s = "Тринадцать";
+                        }
+                        break;
+
+                    case 14:
+                        {
+                            s = "Четырнадцать";
+                        }
+                        break;
+
+                    case 15:
+                        {
+                            s = "Пятнадцать";
+                        }
+                        break;
+
+                    case 16:
+                        {
+                            s = "Шестнадцать";
+                        }
+                        break;
+
+                    case 17:
+                        {
+                            s = "Семнадцать";
+                        }
+                        break;
+
+                    case 18:
+                        {
+                            s = "Восемнадцать";
+                        }
+                        break;
+
+                    case 19:
+                        {
+                            s = "Девятнадцать";
+                        }
+                        break;
+
+
+
+                }
+            }
+            else
+                switch (q)
+                {
+                    case 2:
+                        s = "Двадцать ";
+                        break;
+                    case 3:
+                        s = "Тридцать ";
+                        break;
+                    case 4:
+                        s = "Сорок ";
+                        break;
+                    case 5:
+                        s = "Пятьдесят ";
+                        break;
+                    case 6:
+                        s = "Шестьдесят ";
+                        break;
+                    case 7:
+                        s = "Семьдесят ";
+                        break;
+                    case 8:
+                        s = "Восемьдесят ";
+                        break;
+                    case 9:
+                        s = "Девяноста ";
+                        break;
+                }
+            switch (w)
+            {
+                case 1:
+                    s = "Один ";
+                    break;
+
+                case 2:
+                    s = "Два ";
+                    break;
+                case 3:
+                    s = "Три ";
+                    break;
+                case 4:
+                    s = "Четыре ";
+                    break;
+                case 5:
+                    s = "Пять ";
+                    break;
+                case 6:
+                    s = "Шесть ";
+                    break;
+                case 7:
+                    s = "Семь ";
+                    break;
+                case 8:
+                    s = "Восемь ";
+                    break;
+                case 9:
+                    s = "Девять ";
+                    break;
+            }
+            return s;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
